44f4d55d8f7d4a55741b96988b53797688050b4f
Shawn Dempster <dempster.s.p@gmail.com>
2017-04-08 16:10:51 -0400

    Add parallel coordinates to the scorecard

--- src/components/routes/app/dashboard/reports/views/Scorecard.jsx
+++ src/components/routes/app/dashboard/reports/views/Scorecard.jsx
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import * as d3 from 'd3';
-import * as papa from 'papaparse';
+
 import props from 'deep-property';
 
 const stats = [
@@ -24,41 +24,75 @@ class Scorecard extends Component {
         data: PropTypes.object.isRequired
     };
 
-    renderScorecard(svg, data){
+    static renderScorecard(svg, scores){
         if (svg === null) {
             return;
         }
-        const margin = {top: 30, right: 10, bottom: 10, left: 10},
-            width = 960 - margin.left - margin.right,
+
+        const margin = {top: 50, right: 10, bottom: 10, left: 50},
+            width = 1100 - margin.left - margin.right,
             height = 500 - margin.top - margin.bottom;
 
-        const x = d3.scale.ordinal().rangePoints([0, width], 1),
+        let x = d3.scalePoint().range([0, width], 1),
             y = {},
             dragging = {};
 
-        const line = d3.svg.line(),
-            axis = d3.svg.axis().orient("left");
+        const line = d3.line(),
+            axis = d3.axisLeft(y);
+
+        svg = d3.select('svg')/*.append('svg')*/
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        let vals = '';
+        vals += 'Name' + ',';
+        vals += 'Total Sent' + ','  ;
+        vals += 'Average Sent' + ','  ;
+        vals += 'Total Words' + ','  ;
+        vals += 'Average Words' + ','  ;
+        vals += 'Cardinality' + ','  ;
+        vals += 'Sentiment' + ','  ;
+        vals += 'Total Nouns' + ','  ;
+        vals += 'Total Adjectives' + ','  ;
+        vals += 'Total Verbs' + ','  ;
+        vals += 'Total Adverbs' + ',' ;
 
 
-        /*
-         const svg = d3.select("body").append("svg")
-         .attr("width", width + margin.left + margin.right)
-         .attr("height", height + margin.top + margin.bottom)
-         .append("g")
-         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-         */
-        const stats = papa.Papa.unparse(stats);
-        d3.csv(stats, (error, data) => {
+
+        for(let lst in scores) {
+            vals += '\n';
+            vals += lst + ',';
+            vals += scores[lst].result.num_sent_total + ',';
+            vals += scores[lst].result.num_sent_avg + ',';
+            vals += scores[lst].result.num_word_total + ',';
+            vals += scores[lst].result.num_word_avg + ',';
+            vals += scores[lst].result.cardinality + ',';
+            vals += scores[lst].result.sentiment + ',';
+            vals += scores[lst].result.pos.noun + ',';
+            vals += scores[lst].result.pos.adj + ',';
+            vals += scores[lst].result.pos.verb + ',';
+            vals += scores[lst].result.pos.adv + ',';
+        }
+
+        let data = d3.csvParse(vals);
 
             // Extract the list of dimensions and create a scale for each.
-            x.domain(dimensions = d3.keys(data[0]).filter(function(d) {
-                return d !== "name" && (y[d] = d3.scale.linear()
-                        .domain(d3.extent(data, function(p) { return +p[d]; }))
+            let dimensions = [];
+            x.domain(dimensions = d3.keys(data[0]).filter(
+                function(d) {
+                    let xxx = y[d];
+                    return d !== 'Name' && d !== '' && (y[d] = d3.scaleLinear()
+                        .domain(d3.extent(data, function(p) {
+                            let ppp = p;
+                            return +p[d];
+                        }))
                         .range([height, 0]));
             }));
 
             // Add grey background lines for context.
-            const background = svg.append("g")
+            const background = svg.append('g')
                 .attr("class", "background")
                 .selectAll("path")
                 .data(data)
@@ -66,85 +100,92 @@ class Scorecard extends Component {
                 .attr("d", path);
 
             // Add blue foreground lines for focus.
-            const foreground = svg.append("g")
-                .attr("class", "foreground")
-                .selectAll("path")
+            const foreground = svg.append('g')
+                .attr('class', 'foreground')
+                .selectAll('path')
                 .data(data)
-                .enter().append("path")
-                .attr("d", path);
+                .enter().append('path')
+                .attr('d', path);
 
             // Add a group element for each dimension.
-            const g = svg.selectAll(".dimension")
+            const g = svg.selectAll('.dimension')
                 .data(dimensions)
-                .enter().append("g")
-                .attr("class", "dimension")
-                .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
-                .call(d3.behavior.drag()
-                    .origin(function(d) { return {x: x(d)}; })
-                    .on("dragstart", function(d) {
+                .enter().append('g')
+                .attr('class', 'dimension')
+                .attr('transform', function(d) {
+                    let xd = x(d);
+                    return 'translate(' + x(d) + ')';
+                });
+                /*.call(d3.drag()
+                    .subject(function(d) { return {x: x(d)}; })
+                    .on('start', function(d) {
                         dragging[d] = x(d);
-                        background.attr("visibility", "hidden");
+                        background.attr('visibility', 'hidden');
                     })
-                    .on("drag", function(d) {
+                    .on('drag', function(d) {
                         dragging[d] = Math.min(width, Math.max(0, d3.event.x));
-                        foreground.attr("d", path);
+                        foreground.attr('d', path);
                         dimensions.sort(function(a, b) { return position(a) - position(b); });
                         x.domain(dimensions);
-                        g.attr("transform", function(d) { return "translate(" + position(d) + ")"; })
+                        g.attr('transform', function(d) { return 'translate(' + position(d) + ')'; })
                     })
-                    .on("dragend", function(d) {
+                    .on('end', function(d) {
                         delete dragging[d];
-                        transition(d3.select(this)).attr("transform", "translate(" + x(d) + ")");
-                        transition(foreground).attr("d", path);
+                        transition(d3.select(this)).attr('transform', 'translate(' + x(d) + ')');
+                        transition(foreground).attr('d', path);
                         background
-                            .attr("d", path)
+                            .attr('d', path)
                             .transition()
                             .delay(500)
                             .duration(0)
-                            .attr("visibility", null);
-                    }));
+                            .attr('visibility', null);
+                    }));*/
 
             // Add an axis and title.
-            g.append("g")
-                .attr("class", "axis")
+            g.append('g')
+                .attr('class', 'axis')
                 .each(function(d) { d3.select(this).call(axis.scale(y[d])); })
-                .append("text")
-                .style("text-anchor", "middle")
-                .attr("y", -9)
+                .append('text')
+                .style('text-anchor', 'middle')
+                .attr('y', -9)
                 .text(function(d) { return d; });
 
             // Add and store a brush for each axis.
-            g.append("g")
-                .attr("class", "brush")
+            /*
+            g.append('g')
+                .attr('class', 'brush')
                 .each(function(d) {
-                    d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brushstart", brushstart).on("brush", brush));
+                    d3.select(this).call(d3.brushY()
+                        .extent([0,0],[width,height])
+                        .on("brush", brush));
                 })
-                .selectAll("rect")
-                .attr("x", -8)
-                .attr("width", 16);
-        });
-
+                .selectAll('rect')
+                .attr('x', -8)
+                .attr('width', 16);
+            */
+/*
         function position(d) {
-            var v = dragging[d];
+            let v = dragging[d];
             return v == null ? x(d) : v;
         }
 
         function transition(g) {
             return g.transition().duration(500);
         }
-
+*/
         // Returns the path for a given data point.
         function path(d) {
-            return line(dimensions.map(function(p) { return [position(p), y[p](d[p])]; }));
+            return line(dimensions.map(function(p) { return [x(p), y[p](d[p])]; }));
         }
-
+/*
         function brushstart() {
             d3.event.sourceEvent.stopPropagation();
         }
-
+*/
         // Handles a brush event, toggling the display of foreground lines.
+
         function brush() {
-            var actives = dimensions.filter(function(p) { return !y[p].brush.empty(); }),
+            let actives = dimensions.filter(function(p) { return !y[p].brush.empty(); }),
                 extents = actives.map(function(p) { return y[p].brush.extent(); });
             foreground.style("display", function(d) {
                 return actives.every(function(p, i) {
@@ -152,46 +193,38 @@ class Scorecard extends Component {
                 }) ? null : "none";
             });
         }
+
+        function brushended() {
+            if (!d3.event.sourceEvent) {return;} // Only transition after input.
+            if (!d3.event.selection) {return;} // Ignore empty selections.
+            var d0 = d3.event.selection.map(x.invert),
+                d1 = d0.map(d3.timeDay.round);
+
+            // If empty when rounded, use floor & ceil instead.
+            if (d1[0] >= d1[1]) {
+                d1[0] = d3.timeDay.floor(d0[0]);
+                d1[1] = d3.timeDay.offset(d1[0]);
+            }
+
+            d3.select(this).transition().call(d3.event.target.move, d1.map(x));
+        }
+
     }
     render() {
 
         const { collections } = this.props.data;
 
-        const collectionNames = Object.keys(collections);
-
+        const collectionName = 'Scorecard';
         return (
-            <div className="word-cloud">
-                {Object.keys(collections).map((collectionName) => (
+
+            <div className="scorecard">
+
                     <div key={collectionName} className="scorecard__item">
                         <h2 className="word-cloud__label">{collectionName}</h2>
-                        <svg ref={(svg) => { Scorecard.renderScorecard(svg, collections[collectionName].result.scorecard); }} />
+                        <svg ref={(svg) => { Scorecard.renderScorecard(svg, collections); }} />
                     </div>
-                ))}
+
             </div>
-            /*
-             <div className="scorecard">
-             <table className="table">
-             <thead className="table__head">
-             <tr className="table__row">
-             <td className="table__head-cell table__head-cell--small" />
-             {collectionNames.map((collectionName) => (
-             <th key={collectionName} className="table__head-cell">{collectionName}</th>
-             ))}
-             </tr>
-             </thead>
-             <tbody className="table__body">
-             {stats.map((s) => (
-             <tr key={s} className="table__row">
-             <th className="table__head-cell table__head-cell--body table__head-cell--right">{s.label}</th>
-             {collectionNames.map((collectionName) => (
-             <td key={collectionName} className="table__cell">{props.get(collections[collectionName].result, s.key)}</td>
-             ))}
-             </tr>
-             ))}
-             </tbody>
-             </table>
-             </div>
-             */
         );
     }
 }
