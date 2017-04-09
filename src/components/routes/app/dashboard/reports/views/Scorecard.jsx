9cf9cdc2e1c063c079804cb5ad483c05d70ac893
Kyle Maguire <kylepixel@gmail.com>
2017-04-09 11:25:03 -0400

    Properly format and label scorecard values.

--- src/components/routes/app/dashboard/reports/views/Scorecard.jsx
+++ src/components/routes/app/dashboard/reports/views/Scorecard.jsx
@@ -2,20 +2,24 @@ import React, { Component, PropTypes } from 'react';
 import props from 'deep-property';
 
 const stats = [
-    { key: 'num_sent_total', label: 'Total Sent' },
-    { key: 'num_sent_avg', label: 'Average Sent' },
+    { key: 'num_sent_total', label: 'Total Sentences' },
+    { key: 'num_sent_avg', label: 'Average Sentences' },
     { key: 'num_word_total', label: 'Total Words' },
     { key: 'num_word_avg', label: 'Average Words' },
     { key: 'cardinality', label: 'Cardinality' },
     { key: 'sentiment', label: 'Sentiment' },
-    { key: 'pos.noun', label: 'Total Nouns' },
-    { key: 'pos.adj', label: 'Total Adjectives' },
-    { key: 'pos.verb', label: 'Total Verbs' },
-    { key: 'pos.adv', label: 'Total Adverbs' }
+    { key: 'noun', label: 'Total Nouns' },
+    { key: 'adj', label: 'Total Adjectives' },
+    { key: 'verb', label: 'Total Verbs' },
+    { key: 'adv', label: 'Total Adverbs' }
 ];
 
-function formatStat() {
-    
+function formatInteger(number) {
+    return parseInt(number, 10).toLocaleString();
+}
+
+function formatFloat(number) {
+    return (parseInt(number * Math.pow(10, 3), 10) / Math.pow(10, 3)).toLocaleString();
 }
 
 class Scorecard extends Component {
@@ -26,9 +30,26 @@ class Scorecard extends Component {
 
     render() {
 
-        const { collections } = this.props.data;
+        const collections = Object.keys(this.props.data.collections).map((name) => {
+            const { num_sent_total, num_sent_avg, num_word_total, num_word_avg, cardinality, sentiment, pos } = this.props.data.collections[name].result;
+            return {
+                name,
+                result: {
+                    num_sent_total: formatInteger(num_sent_total),
+                    num_sent_avg: formatInteger(num_sent_avg),
+                    num_word_total: formatInteger(num_word_total),
+                    num_word_avg: formatInteger(num_word_avg),
+                    cardinality: formatInteger(cardinality),
+                    sentiment: formatFloat(sentiment),
+                    noun: formatInteger(pos.noun),
+                    adj: formatInteger(pos.adj),
+                    verb: formatInteger(pos.verb),
+                    adv: formatInteger(pos.adv)
+                }
+            };
+        });
 
-        const collectionNames = Object.keys(collections);
+        //const collectionNames = Object.keys(collections);
 
         return (
             <div className="scorecard">
@@ -36,8 +57,8 @@ class Scorecard extends Component {
                     <thead className="table__head">
                         <tr className="table__row">
                             <td className="table__head-cell table__head-cell--small" />
-                            {collectionNames.map((collectionName) => (
-                                <th key={collectionName} className="table__head-cell">{collectionName}</th>
+                            {collections.map((collection) => (
+                                <th key={collection.name} className="table__head-cell">{collection.name}</th>
                             ))}
                         </tr>
                     </thead>
@@ -45,8 +66,8 @@ class Scorecard extends Component {
                         {stats.map((s) => (
                             <tr key={s.key} className="table__row">
                                 <th className="table__head-cell table__head-cell--body table__head-cell--right">{s.label}</th>
-                                {collectionNames.map((collectionName) => (
-                                    <td key={collectionName} className="table__cell">{props.get(collections[collectionName].result, s.key)}</td>
+                                {collections.map((collection) => (
+                                    <td key={collection.name} className="table__cell">{props.get(collection.result, s.key)}</td>
                                 ))}
                             </tr>
                         ))}
