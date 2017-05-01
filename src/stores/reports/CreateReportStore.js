5e51e0e8e71eb2014fef6640b71bb9e0237b61fa
Kyle Maguire <kylepixel@gmail.com>
2017-05-01 19:12:27 -0400

    Fix classify API request. Don't reset initialized state when coming back to main pages.

--- src/stores/reports/CreateReportStore.js
+++ src/stores/reports/CreateReportStore.js
@@ -192,7 +192,7 @@ class CreateReportStore {
                     data.option = clusterOption;
                 }
                 if (analytic === 'classify') {
-                    data.option1 = classifyOption;
+                    data.option = classifyOption;
                 }
 
                 createReport(data)
