f0752b86c8236e833a743590d0ec8baa65235ee8
Shawn Dempster <dempster.s.p@gmail.com>
2017-05-05 18:55:01 -0400

    Update SentimentYear to use the full date and have zooming

--- src/components/routes/app/dashboard/reports/views/SentimentYear.jsx
+++ src/components/routes/app/dashboard/reports/views/SentimentYear.jsx
@@ -1,10 +1,19 @@
 import React, {Component, PropTypes} from 'react';
-import { inject, observer } from 'mobx-react';
+import {inject, observer} from 'mobx-react';
 import * as d3 from 'd3';
 import tip from 'd3-tip';
 d3.tip = tip;
 import * as d3Scale from 'd3-scale-chromatic';
-import * as d3Trans from 'd3-transition';
+
+import {brush, brushSelection}  from 'd3-brush';
+
+import event from 'd3-selection';
+d3.event = event;
+
+import {zoom}  from 'd3-zoom';
+
+
+
 
 const WIDTH = 960;
 const HEIGHT = 400;
@@ -25,11 +34,11 @@ class SentimentYear extends Component {
             let docSent = vals[lst].result.documentsentiment;
             for (let k in docYear) {
 
-                strData += '01-' + '01-' + docYear[k] + ',' + docSent[k] + ',' + k + ',' + lst + '\n';
+                strData += docYear[k] + ',' + docSent[k] + ',' + k + ',' + lst + '\n';
             }
         }
 
-        const parseTime = d3.timeParse('%m-%d-%Y');
+        const parseTime = d3.timeParse('%Y-%m-%d');
         let data = d3.csvParse(strData, function (d) {
             return {
                 date: parseTime(d.date),
@@ -42,9 +51,11 @@ class SentimentYear extends Component {
         /*
          Set sizing
          */
-        const margin = {top: 20, right: 20, bottom: 30, left: 40},
+        const margin = {top: 20, right: 20, bottom: 20, left: 40},
             width = WIDTH - margin.left - margin.right,
             height = HEIGHT - margin.top - margin.bottom;
+
+        let idleTimeout, idleDelay = 350;
 
         /*
          Set x & y scales and create axes
@@ -59,14 +70,10 @@ class SentimentYear extends Component {
 
         const yAxis = d3.axisLeft(y);
 
-        const transition = d3Trans;
         /*
-         Build chart
+         Set functions
          */
-        svg = d3.select('svg')
-            .attr('width', width + margin.left + margin.right)
-            .attr('height', height + margin.top + margin.bottom)
-            .attr('viewBox', `0 0 ${WIDTH} ${HEIGHT}`);
+
 
         const tip = d3.tip()
             .attr('class', 'sentiment-year__tooltip')
@@ -75,50 +82,91 @@ class SentimentYear extends Component {
             .html((d) => {
                 const title = this.props.reports.getDocumentTitle(d.docId);
                 const sentiment = d.sentiment.toFixed(4);
-                return `<span class="sentiment-year__tooltip-title">${title}</span>` +
-                    `<span class="sentiment-year__tooltip-sentiment">${sentiment}</span>`;
+                return `<span class='sentiment-year__tooltip-title'>${title}</span>` +
+                    `<span class='sentiment-year__tooltip-sentiment'>${sentiment}</span>`;
             });
-        svg.call(tip);
 
-        svg = svg.append('g')
-            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+        var brush = d3.brush().on('end', brushended);
 
+        /*
+         Build chart
+         */
+        svg = d3.select('svg')
+            .attr('width', width + margin.left + margin.right)
+            .attr('height', height + margin.top + margin.bottom)
+            .attr('viewBox', `0 0 ${WIDTH} ${HEIGHT}`)
+            .call(tip)
+            .append('g')
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        ;
+
+        //svg.call(tip);
+
+        // Add Detail Chart
+
+
+        /*  =======================
+         Top Chart
+         ======================= */
+        // Set domains
         x.domain(d3.extent(data, function (d) {
             return d.date;
         })).nice();
+
         y.domain([-1, 1]).nice();
 
-        svg.append('g')
-            .attr('class', 'x axis')
-            .attr('transform', 'translate(0,' + height + ')')
-            .call(xAxis)
-            .append('text')
-            .attr('class', 'label')
-            .attr('x', width)
-            .attr('y', -6)
-            .style('text-anchor', 'end')
-            .text('Year');
+
+
+        // Add x Axis
+        const gX = svg.append('g')
+            .attr('class', 'axis axis--x')
+            .attr('transform', 'translate(0,' + (height ) + ')')
+            .call(xAxis);
+
+        // Add y Axis
+        const gY = svg.append('g')
+            .attr('class', 'axis axis--y')
+            //.attr('transform', 'translate(' + margin.left + ',0)')
+            .call(yAxis);
 
         svg.append('g')
-            .attr('class', 'y axis')
-            .call(yAxis)
-            .append('text')
-            .attr('class', 'label')
-            .attr('transform', 'rotate(-90)')
-            .attr('y', 6)
-            .attr('dy', '.71em')
-            .style('text-anchor', 'end')
-            .text('Sentiment')
+            .attr('class', 'brush')
+            //.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+            //.attr('width', (width - margin.left - margin.right))
+            .call(brush)
+            .append('g')
+            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-        svg.selectAll('.point')
+        svg.append('defs').append('clipPath')
+            .attr('id', 'clip').append('rect')
+            .attr('width', width)
+            .attr('height', height);
+
+        // Add points to top chart
+        /*const view = svg.append('rect')
+            .classed('view', true)
+            .attr('width', width)
+            .attr('height', height)
+            .attr('transform', 'translate(' + margin.left + ',' + margin.bottom + ')');*/
+
+        var objects = svg.append('g')
+            .classed('objects', true)
+            .attr('width', width)
+            .attr('height', height)
+            .attr('clip-path', 'url(#clip)');
+
+
+        objects.selectAll('.point')
             .data(data)
-            .enter().append('path')
+            .enter()
+            .append('path')
             .attr('class', function (d) {
                 return 'point ' + d.listName.replace(/\s/g, '');
             })
             .style('fill', function (d) {
                 return colorScale(d.listName);
             })
+            .style('opacity', .75)
             .attr('d', d3.symbol().type(function (d) {
                 if (d.sentiment < 0) {
                     return d3.symbolWye;
@@ -126,14 +174,12 @@ class SentimentYear extends Component {
                     return d3.symbolCircle;
                 }
             }))
-            .attr('transform', function (d) {
-                return 'translate(' + x(d.date) + ',' + y(d.sentiment) + ')';
-            })
+            .attr('transform', transform)
             .style('stroke', function (d) {
                 if (d.sentiment < 0) {
                     return '#585F61';
                 } else {
-                    return '#C1C7C9';//color(d.listName);
+                    return '#C1C7C9';
                 }
             })
             .style('stroke-width', function (d) {
@@ -149,39 +195,77 @@ class SentimentYear extends Component {
             .on('mouseover', tip.show)
             .on('mouseout', tip.hide);
 
-        // Highlights the list
-        function cellover(d) {
-            d3.selectAll('.' + d.replace(/\s/g, ''))
-                .style('stroke-width', 2)
-                .size(128);
+        function zoomed() {
+            const xz = d3.event.transform.rescaleX(x);
+            gX.call(xAxis.scale(xz));
+
+            svg.selectAll('.point')
+                .attr('transform', transform)
+            ;
         }
 
-        // Sets width back to regular
-        function cellout(d) {
-            d3.selectAll('.' + d.replace(/\s/g, ''))
-                .style('stroke-width', 1);
+        function brushended() {
+            var s = d3.event.selection;
+            if (!s) {
+                if (!idleTimeout) {
+
+                return idleTimeout = setTimeout(idled, idleDelay)
+            }
+                const xDom =d3.extent(data, function (d) {
+                    return d.date;
+                });
+                x.domain(xDom).nice();
+                y.domain([-1,1]).nice();
+            } else {
+                x.domain([s[0][0], s[1][0]].map(x.invert, x));
+                y.domain([s[1][1], s[0][1]].map(y.invert, y));
+                svg.select('.brush').call(brush.move, null);
+            }
+            zoom();
+        }
+        function idled() {
+            idleTimeout = null;
+        }
+        function zoom() {
+            var t = svg.transition().duration(750);
+            svg.select('.axis--x').transition(t).call(xAxis);
+            svg.select('.axis--y').transition(t).call(yAxis);
+            svg.selectAll('.point').transition(t)
+                .attr('transform', transform)
+        }
+
+        function resetted() {
+            svg.transition()
+                .duration(750)
+                .call(zoom.transform, d3.zoomIdentity);
+        }
+
+        function transform(d) {
+            return 'translate(' + x(d.date) + ',' + y(d.sentiment) + ')';
         }
 
     }
 
     render() {
 
-        const { collections } = this.props.data;
+        const {collections} = this.props.data;
 
         const colorScale = d3.scaleOrdinal(d3Scale.schemeSet1)
             .domain(Object.keys(collections));
 
         return (
-            <div className="sentiment-year">
-                <ul className="view-report__legend">
+            <div className='sentiment-year'>
+                <ul className='view-report__legend'>
                     {Object.keys(collections).map((collectionName) => (
-                        <li key={collectionName} className="view-report__legend-item">
-                            <div className="view-report__legend-shape" style={{ background: colorScale(collectionName) }}></div>
+                        <li key={collectionName} className='view-report__legend-item'>
+                            <div className='view-report__legend-shape'
+                                 style={{background: colorScale(collectionName)}}></div>
                             <span>{collectionName}</span>
                         </li>
                     ))}
                 </ul>
-                <svg className="sentiment-year__chart" ref={(svg) => this.renderSentimentYear(svg, collections, colorScale)} />
+                <svg className='sentiment-year__chart'
+                     ref={(svg) => this.renderSentimentYear(svg, collections, colorScale)}/>
             </div>
         );
     }
